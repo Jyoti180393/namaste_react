571c3a6ad176310ce6d83ce3c12c9d8c58f2073f
Jyoti180393 <pokhariajyoti18@gmail.com>
2025-11-28 14:28:05 +0530

    Laying foundation code

---
+++ app1.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading = React.createElement("h1", {id: "heading"}, "Hello World from React ");
+root.render(heading);
+console.log(heading); // This will show object structure of heading
+
+const parent = React.createElement("div", {id:"parent"},
+  [
+    React.createElement("div",{id:"child"}, [ 
+      React.createElement("h1",{},"I am a H1 tag"),
+      React.createElement("h2",{},"I am a H2 tag")
+    ]),
+    React.createElement("div",{id:"child2"},[
+      React.createElement("h1",{},"I am a H1 tag from "),
+      React.createElement("h2",{},"I am a H2 tag from child2")]
+  )
+  ] 
+    );
+// root.render(parent);
+console.log(parent); // This will show object structure of parent
