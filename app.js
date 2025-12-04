5e7ab04e344aee6b4e8359eceb67478badc48a65
Jyoti180393 <pokhariajyoti18@gmail.com>
2025-12-04 05:36:27 +0530

    talk is cheap code and assignment

--- app.js
+++ app.js
@@ -1,40 +1,90 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+const resObj = {
+  data: [
+    {
+      restName: "Meghana Foods",
+      cuisine: ["Biryani", "North - Indian", "Mughal"],
+      rating: 4.5,
+      estinameDeliveryTime: "30 min",
+      costForTwo: 400,
+    },
+    {
+      restName: "Kfc",
+      cuisine: ["Burger", "Nuggets", "Fastfood"],
+      rating: 4.4,
+      estinameDeliveryTime: "25 min",
+      costForTwo: 350,
+    },
+  ],
+};
 
-const span = <span> Hello from Span Element</span>;
-const heading = (
-<h1 id="heading" className="heading"> 
-  Hello from React Element
-  {span} 
-  </h1>
-)
+// react component using JSX
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+// const styleCard = {
+//   backgroundColor: "#f0f0f0",
+// };
+const RestaurantCard = (props) => {
+  console.log(props);
+  const { resData } = props;
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        alt="res-logo"
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0vvulfbahjxjz6k4uwi"
+      ></img>
+      <h3>{resData.restName}</h3>
+      <h4>{resData.cuisine.join(", ")}</h4>
+      <h4>{resData.rating} stars</h4>
+      <h4>{resData.estinameDeliveryTime}</h4>
+      <h4>{resData.costForTwo} for Two</h4>
+    </div>
+  );
+};
 
-// this will be rendered in root as Html Element by ReactDOM
-// console.log(heading);
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restaurant-container">
+        <RestaurantCard resData={resObj.data[0]} />
+        <RestaurantCard resData={resObj.data[1]} />
+      </div>
+    </div>
+  );
+};
 
-
-
-// UsingJSX
-const Heading1 = () => (
-  <h1 id="jsx-heading" className="jsxHeading">
-    This is a React Element using JSX
-  </h1>
-);
-//JSX transpiled to React.createElement() by Babel
-
-// React component with JSX
-const HeadingComponent = () => (
-  <div id="container">
-    {heading}
-    <Heading1/>
-    <h1>This is react Component</h1>
-  </div>
-)
-console.log(< HeadingComponent />);
-// console.log(jsxHeading);
+console.log(<RestaurantCard />);
+const element = <h1>Hello World!</h1>; // React Element using JSX
+const AppLayout = () => {
+  return (
+    <div id="app-layout" className="app">
+      {element}
+      {<Header />}
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// to render a React component we need angular brackets inside render method
-// with <> babbel understand that it is a React component
-root.render(<HeadingComponent />);
-
+root.render(<AppLayout />);
--- app1.js
+++ app1.js
@@ -1,23 +1,59 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
-
+import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React ");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World from React "
+);
 root.render(heading);
 console.log(heading); // This will show object structure of heading
 
-const parent = React.createElement("div", {id:"parent"},
-  [
-    React.createElement("div",{id:"child"}, [ 
-      React.createElement("h1",{},"I am a H1 tag"),
-      React.createElement("h2",{},"I am a H2 tag")
-    ]),
-    React.createElement("div",{id:"child2"},[
-      React.createElement("h1",{},"I am a H1 tag from "),
-      React.createElement("h2",{},"I am a H2 tag from child2")]
-  )
-  ] 
-    );
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am a H1 tag"),
+    React.createElement("h2", {}, "I am a H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am a H1 tag from "),
+    React.createElement("h2", {}, "I am a H2 tag from child2"),
+  ]),
+]);
 // root.render(parent);
 console.log(parent); // This will show object structure of parent
+
+const span = <span> Hello from Span Element</span>;
+const heading1 = (
+  <h1 id="heading" className="heading">
+    Hello from React Element
+    {span}
+  </h1>
+);
+
+// this will be rendered in root as Html Element by ReactDOM
+// console.log(heading);
+
+// UsingJSX
+const Heading1 = () => (
+  <h1 id="jsx-heading" className="jsxHeading">
+    This is a React Element using JSX
+  </h1>
+);
+//JSX transpiled to React.createElement() by Babel
+
+// React component with JSX
+const HeadingComponent = () => (
+  <div id="container">
+    {heading}
+    <Heading1 />
+    <h1>This is react Component</h1>
+  </div>
+);
+console.log(<HeadingComponent />);
+// console.log(jsxHeading);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// to render a React component we need angular brackets inside render method
+// with <> babbel understand that it is a React component
+root.render(<HeadingComponent />);
