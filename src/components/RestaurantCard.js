af1fc62d795ff3cbf610c4235de1ec7b8c7af64b
Jyoti180393 <pokhariajyoti18@gmail.com>
2025-12-15 15:35:53 +0530

    Chapter 6 - assignment

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -25,7 +25,7 @@ const RestaurantCard = (props) => {
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
       <h4>{estinameDeliveryTime}</h4>
-      <h4>{costForTwo} for Two</h4>
+      <h4>{costForTwo}</h4>
     </div>
   );
 };
